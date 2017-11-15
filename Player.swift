c02570f3614bd86eaa3b8a0d401baf876a8913b0
August <august.lin.usa@gmail.com>
2017-11-15 14:42:09 -0800

    Add Player Model

---
+++ Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by August on 11/15/17.
+//  Copyright © 2017 August. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var name: String?
+    var game: String?
+    var rating: Int
+}
