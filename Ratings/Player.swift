21a6d7fc86a22dbd9b137c5c1d1b50abcb6de66e
August <august.lin.usa@gmail.com>
2017-11-20 16:19:58 -0800

    Add TableViewController and Model

--- Ratings/Player.swift
+++ Ratings/Player.swift
@@ -2,13 +2,15 @@
 //  Player.swift
 //  Ratings
 //
-//  Created by August on 11/15/17.
+//  Created by August on 11/20/17.
 //  Copyright © 2017 August. All rights reserved.
 //
 
 import Foundation
 
 struct Player {
+    
+    // MARK: - Properties
     var name: String?
     var game: String?
     var rating: Int
--- Ratings/SampleData.swift
+++ Ratings/SampleData.swift
@@ -2,7 +2,7 @@
 //  SampleData.swift
 //  Ratings
 //
-//  Created by August on 11/15/17.
+//  Created by August on 11/20/17.
 //  Copyright © 2017 August. All rights reserved.
 //
 
