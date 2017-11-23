e04c61fddd04fc3737bf45e26f6f075d8bb2163f
August <august.lin.usa@gmail.com>
2017-11-22 16:40:02 -0800

    Implement unwind Segue

--- Ratings/PlayerDetailsViewController.swift
+++ Ratings/PlayerDetailsViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class PlayerDetailsViewController: UITableViewController {
+    // MARK: - Properties
+    var player: Player?
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var detailLabel: UILabel!
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)  {
+        if segue.identifier == "SavePlayerDetail",
+            let playerName = nameTextField.text {
+            player = Player(name: playerName, game: "Chess", rating: 1)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
 }
+
+
 
 
 // MARK: - UITableViewDelegate
